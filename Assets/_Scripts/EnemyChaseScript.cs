df4b5f428198d5dcda5701d7efb8d3b2a2f7dbb7
wil <wiluniversityacc@gmail.com>
2025-08-25 14:28:31 +0700

    Enemy LOS

--- Assets/_Scripts/EnemyChaseScript.cs
+++ Assets/_Scripts/EnemyChaseScript.cs
@@ -1,18 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyChaseScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 2f;
+    public float detectionRange = 15f;
+
+    public Transform target;
+
+    SpriteRenderer spriteRenderer;
+    NavMeshAgent agent;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.CompareTag("LOS"))
+        {
+            chasePlayers();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+
+        if (target != null)
+        {
+            Vector3 origin = transform.position;
+            bool clear = LineOfSight(target);
+
+            Gizmos.color = clear ? Color.green : Color.yellow;
+            Gizmos.DrawLine(origin, target.position);
+        }
+    }
+
+    void chasePlayers()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRange, LayerMask.GetMask("Player"));
+        foreach (Collider2D hit in hits)
+        {
+            PlayerInputScript player = hit.GetComponent<PlayerInputScript>();
+            if (player != null)
+            {
+                if (LineOfSight(player.transform))
+                {
+                    target = player.transform;
+                    UpdateBehaviour();
+                    break;
+                }
+                else
+                {
+                    target = null;
+                    agent.SetDestination(transform.position);
+                    agent.isStopped = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    public bool LineOfSight(Transform target)
+    {
+        LayerMask obstacleLayer = LayerMask.GetMask("Obstacle");
+        Vector2 origin = transform.position;
+        Vector2 direction = (Vector2)target.position - origin;
+        float distance = Vector2.Distance(origin, target.position);
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction.normalized, distance, obstacleLayer);
+        return hit.collider == null;
+    }
+
+    void UpdateBehaviour()
+    {
+        if (target != null)
+        {
+            agent.isStopped = false;
+            agent.speed = speed;
+            agent.SetDestination(target.position);
+        }
     }
 }
